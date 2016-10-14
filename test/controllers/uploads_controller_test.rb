ac55ebca50a7d0b5818fe58d926c74d126c65546
ZengWeimao <so.zengtao@gmail.com>
2016-10-14 18:31:09 +0800

    try test controller

---
+++ test/controllers/uploads_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class UploadsControllerTest < ActionController::TestCase
+
+  test "POST create" do
+    # post :create, params: {
+    #   first_name: 'ztao',
+    #   last_name: 'tao',
+    #   annual_salary: 100000,
+    #   super_rate: '10%',
+    #   start_date: '1 March'
+    #  }
+    # puts response
+    # assert_response :success
+  end
+
+end
